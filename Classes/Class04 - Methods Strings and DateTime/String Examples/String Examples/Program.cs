bcb2744449ef2673862793196a18cfcb5d35cbeb
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-09-28 09:16:31 +0200

    addded

---
+++ Classes/Class04 - Methods Strings and DateTime/String Examples/String Examples/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace String_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine();
+        }
+    }
+}
