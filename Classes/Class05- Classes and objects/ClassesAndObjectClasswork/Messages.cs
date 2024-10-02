318fc633696e93306e9e175588a00c8660856430
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-10-02 17:40:20 +0200

    added new file

---
+++ Classes/Class05- Classes and objects/ClassesAndObjectClasswork/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjectClasswork
+{
+    public static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello welcome to the program");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for somrthing");
+        }
+        public static void Bye()
+        {
+            Console.WriteLine("Bye thanks for visiting");
+        }
+
+    }
+}
---
+++ Classes/Class05- Classes and objects/ClassesAndObjectClasswork/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ClassesAndObjectClasswork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+        }
+    }
+
+    class Human
+    {
+        string name;
+        int age;
+
+        void Eat()
+        {
+            Console.WriteLine(name + "is eating");
+        }
+        void Sleep()
+        {
+            Console.WriteLine(name + "is eating");
+        }
+
+
+
+
+    }
+}
