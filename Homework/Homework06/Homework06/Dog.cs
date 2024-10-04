5300d5ca2bea4f26203e4bef1ef0b573ce720a97
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-10-04 12:22:20 +0200

    added homework 06

---
+++ Homework/Homework06/Homework06/Dog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework06
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+
+        public Dog() 
+        { 
+
+        }
+        public Dog(string name,string breed,string color)
+        {
+            Name = name;
+            Breed = breed;
+            Color = color;
+            
+        }
+
+        public string Eat()
+        {
+            return "Dog is now eating";
+        }
+        public  string Play()
+        {
+            return "Dog is now playing";
+        }
+        public  string ChasingTail()
+        {
+            return "Dog is now chasing it's tail";
+        }
+    }
+}
---
+++ Homework/Homework06/Homework06/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace Homework06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DogApp");
+            Console.WriteLine("Please enter the dogs: name,breed and color");
+            
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+
+            Dog dog1 = new Dog(input1,input2,input3);
+
+            Console.WriteLine("\n" + dog1.Name);
+            Console.WriteLine(dog1.Breed);
+            Console.WriteLine(dog1.Color);
+
+            Console.WriteLine("Please enter a number form 1 to 3 to chose which action you want your dog to perform" +
+                "1: Eating // 2.Playing // 3.ChasingTail");
+
+            string input4 = Console.ReadLine();
+            if(int.TryParse(input4 , out int action))
+            {
+                if (action == 1)
+                {
+                    Console.WriteLine("you chose the eat option");
+                    Console.WriteLine(dog1.Eat());
+                   
+                }
+                else if (action == 2) {
+                    Console.WriteLine("you chose the play option");
+                    Console.WriteLine(dog1.Play());
+                }
+                else  if (action == 3)
+                {
+                    Console.WriteLine("you chose the chasing tail option option");
+                    Console.WriteLine(dog1.ChasingTail());
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice: Please enter 1,2 or 3");
+                }
+            };
+
+            //switch(play2)
+            //{
+            //    case 0:
+            //}
+
+
+        }
+    }
+}
