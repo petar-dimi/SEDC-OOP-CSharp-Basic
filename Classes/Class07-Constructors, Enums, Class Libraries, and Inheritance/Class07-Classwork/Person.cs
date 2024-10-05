33be8ccefc98283111117d1427fea83584a58623
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-10-05 13:59:57 +0200

    added class07 classwok01

---
+++ Classes/Class07-Constructors, Enums, Class Libraries, and Inheritance/Class07-Classwork/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class07_Classwork
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        private long AccountNumber { get; set; }
+
+        public Person()
+        {
+            AccountNumber = 7;
+        }
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+            AccountNumber = 8;
+            
+        }
+        public void Talk(string text)
+        {
+            Console.WriteLine($"Human{Name} is saying {text}");
+        }
+
+    }
+}
---
+++ Classes/Class07-Constructors, Enums, Class Libraries, and Inheritance/Class07-Classwork/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Class07_Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Person person1 = new Person("Gabriel", 20);
+
+            person1.Talk("hello");
+
+            
+        }
+    }
+}
