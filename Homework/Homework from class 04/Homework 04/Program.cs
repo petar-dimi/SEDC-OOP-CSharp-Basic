f5a1ff9a80f204e5a0ec2c82b7429bca8b2f6a96
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-10-24 16:29:02 +0200

    update

---
+++ Homework/Homework from class 04/Homework 04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = new int[6];
+
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number:.{i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            
+            int sumOfEvens = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)  
+                {
+                    sumOfEvens += number;
+                }
+            }
+
+            
+            Console.WriteLine($"The result is: {sumOfEvens}");
+        }
+    }
+}
